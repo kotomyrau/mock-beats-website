7adc001790b0efe6e8c86f02573d646939a0b702
Kotomyra Uchida <kotomyrau@gmail.com>
2021-04-28 13:18:38 +0100

    json file changed

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import ProductData from '../data/products.json'
 
 class ProductPage extends Component {
   render() {    
     return (
       <div>
+        {ProductData.map((productDetail, index) => {
+          return <div index={index}>
+            <h1>{productDetail.name} {index}</h1>
             
+          </div>
+        })}
       </div>
     )
   }
