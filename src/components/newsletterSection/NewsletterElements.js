289f62ec0b24b232b37ae707f5ec3037e81e5810
Kotomyra Uchida <kotomyrau@gmail.com>
2021-04-28 20:32:52 +0100

    added css for newsletter form

--- src/components/newsletterSection/NewsletterElements.js
+++ src/components/newsletterSection/NewsletterElements.js
@@ -1,5 +1,50 @@
 import styled from 'styled-components'
 
 export const NewsletterContainer = styled.div`
-    margin: 4vw 0;
+  text-align: center;
+  margin: 14vh 0;
 `
+export const FormWrapper = styled.div`
+  margin: 0 auto;
+  width: fit-content;
+`
+
+export const FormHeader = styled.h1`
+  color: #2B2835;
+  font-size: 50px;
+  font-weight: 700;
+  margin: 0;
+`
+export const FormSubheader = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  margin: 2vh 0;
+`
+export const FormContainer = styled.div`
+
+`
+export const Form = styled.form`
+
+` 
+export const Input = styled.input`
+  border: 1px solid #BFBFBF;
+  border-radius: 50px;
+  height: 36px;
+  margin: 4vh 0 3vh;
+  outline: none;
+  padding: 1vh 2vw;
+  width: 100%;
+` 
+
+export const FormButton = styled.input`
+  background-color: #FFC700;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: 600;
+  height: 41px;
+  padding: 1vh 2vw;
+  text-transform: uppercase;
+  width: 100%;
+` 
--- src/components/newsletterSection/index.js
+++ src/components/newsletterSection/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { NewsletterContainer } from './NewsletterElements'
+import { NewsletterContainer, FormWrapper, FormHeader, FormSubheader, FormContainer, Form, FormLabel, Input, FormButton} from './NewsletterElements'
 
 const Newsletter = () => {
   return (
     <NewsletterContainer>
-
-
+      <FormWrapper>
+        <FormHeader>
+          Hear it first
+        </FormHeader>
+        <FormSubheader>
+          Get updates on product drops, collaborations and all things Beats. 
+        </FormSubheader>
+          <FormContainer>
+            <Form class="newsletter-form" onSubmit="#">
+                <Input placeholder="Enter your email" required='true' /><br/>
+              <FormButton type="submit" value="Sign Up" />
+            </Form>
+          </FormContainer>
+      </FormWrapper>
     </NewsletterContainer>
   )
 }
