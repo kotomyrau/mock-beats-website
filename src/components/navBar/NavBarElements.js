0f5e2b271599afe72709f8a9a6a68b7ded48365a
Kotomyra Uchida <kotomyrau@gmail.com>
2021-04-28 15:19:16 +0100

    added navbar layout'

--- src/components/navBar/NavBarElements.js
+++ src/components/navBar/NavBarElements.js
@@ -2,36 +2,45 @@ import styled from 'styled-components'
 
 export const Nav = styled.nav`
   background-color: rgba(25, 23, 32, 0.8);
-  height: 78px;
+  box-shadow: 0px 0px 4px 2px #808080;
+  height: auto;
+  position: sticky;
+  top: 0;
+  width: 100%;
 `
 export const NavContainer = styled.div`
   display: flex;
+  height: 78px;
   justify-content: space-between;
-  margin: auto 8vw;
+  margin: 0 8vw;
   `
   
-  export const NavLogo = styled.div`
-
+export const NavLogo = styled.div`
+  margin: auto 0;
 `
 export const Img = styled.img`
   height: 50px;
 `
 export const NavMenu = styled.div`
-width: 70vw;
-text-align: center;
+  height: fit-content;
+  margin: auto 0;
 `
+
 export const NavItem = styled.div`
-display: inline-block;
+  display: inline-block;
+  margin: 0 4vw;
 `
 export const NavLink = styled.a`
-  color: white;
+  color: #FFFFFF;
+  font-size: 15px;
+  font-weight: medium;
+  text-transform: uppercase;
 `
 export const NavIcons = styled.div`
-
+  margin: auto 0;
 `
 
 export const IconImg = styled.img`
-height: 20px;
-
-
+  padding: 0 18px;
+  height: 20px;
 `
