2a5e51167d291a2931b313798b922e42c3f821e4
Kotomyra Uchida <kotomyrau@gmail.com>
2021-04-28 20:57:08 +0100

    created basic footer grid layout

--- src/components/footer/FooterElements.js
+++ src/components/footer/FooterElements.js
@@ -1,6 +1,44 @@
 import styled from 'styled-components'
 
 export const FooterContainer = styled.div`
-height: 10vh;
-background-color: pink;
+height: auto;
+background-color: red;
 `
+
+export const FooterGrid = styled.div`
+  display: grid;
+  // background-color: red;
+  grid-template-areas: 
+    "col1 col2 col3 col4 col5";
+  grid-template-rows: 1fr;
+  grid-template-columns: 0.6fr 1fr 1.2fr 1fr 1.2fr;
+  margin: 0 8vw;
+  `
+export const FooterLogo = styled.div`
+  grid-area: col1;
+  background-color: pink;
+`
+export const FooterImg = styled.img`
+
+`
+export const SiteMapProducts = styled.div`
+grid-area: col2;
+background-color: orange;
+`
+
+export const SiteMapSupport = styled.div`
+grid-area: col3;
+background-color: yellow;
+`
+
+export const SiteMapCompany = styled.div`
+grid-area: col4;  
+background-color: blue;
+`
+
+export const FooterSocials = styled.div`
+grid-area: col5;
+background-color: green;
+`
+
+
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import { FooterContainer } from './FooterElements'
-
+import { FooterContainer, FooterGrid, FooterLogo, FooterImg, SiteMapProducts, SiteMapSupport, SiteMapCompany, FooterSocials } from './FooterElements'
+import logo from "../../images/logo.png";
 const Footer = () => {
-    return (
-        <FooterContainer>
-            
-        </FooterContainer>
-    )
+  return (
+    <FooterContainer>
+      <FooterGrid>
+        <FooterLogo>
+          <FooterImg src={logo} alt="beats logo"/>        
+        </FooterLogo>
+        <SiteMapProducts>
+          products
+        </SiteMapProducts>
+        <SiteMapSupport>
+          support
+        </SiteMapSupport>
+        <SiteMapCompany>
+          company
+        </SiteMapCompany>
+        <FooterSocials>
+          links
+        </FooterSocials>
+      </FooterGrid>
+    </FooterContainer>
+  )
 }
 
 export default Footer
