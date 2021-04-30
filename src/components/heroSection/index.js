05786ca84b540a51deb75e8bb3e7e370b86b2f06
Kotomyra Uchida <kotomyrau@gmail.com>
2021-04-30 10:40:36 +0100

    fixed changes on safari

--- src/components/heroSection/index.js
+++ src/components/heroSection/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { HeroContainer, HeroImgWrapper, HeroImg, HeroTextWrapper, HeroSubHeader, HeroHeader, ColorHeader, FindColor, ColorOptions, ColorOption1, ColorOption2, ColorOption3, HeroDetails, ProductPrice, ProductButton, HeroOffer} from './HeroSectionElements';
 import "../../components/buttons/buttons.css"
 
-const HeroSection = ({id, leftImg, img, alt, header, subHeader, colorHeader, findColor, colorOptions, productPrice, productOffer}) => {
+const HeroSection = ({id, leftImg, img, alt, header, headerSup, subHeader, colorHeader, findColor, colorOptions, productPrice, productOffer}) => {
   return (
     <HeroContainer id={id} leftImg={leftImg}>
       <HeroImgWrapper leftImg={leftImg}>
@@ -10,7 +10,7 @@ const HeroSection = ({id, leftImg, img, alt, header, subHeader, colorHeader, fin
       </HeroImgWrapper>
       <HeroTextWrapper leftImg={leftImg}>
         <HeroSubHeader>{subHeader}</HeroSubHeader>
-        <HeroHeader leftImg={leftImg}>{header}</HeroHeader>
+        <HeroHeader leftImg={leftImg}>{header}<sup>{headerSup}</sup></HeroHeader>
           <ColorHeader>{colorHeader}</ColorHeader>
           <FindColor>{findColor}</FindColor>
           <ColorOptions colorOptions={colorOptions}>
@@ -27,7 +27,7 @@ const HeroSection = ({id, leftImg, img, alt, header, subHeader, colorHeader, fin
       <HeroOffer leftImg={leftImg}> {productOffer}</HeroOffer>
     </HeroTextWrapper>
   </HeroContainer>
-  )
+  ) 
 }
 
 export default HeroSection
